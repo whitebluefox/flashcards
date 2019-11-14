4b45430e4a2b240c8476d804fcb2f31906c6d01c
whitebluefox <kalinkina.nika@mail.ru>
2019-11-14 18:44:15 +0300

    add validates, make create,update,delete card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -9,18 +9,47 @@ class CardsController < ApplicationController
     @card = Card.find(params[:id])
   end
 
-  def new; end
+  def new
+    @card = Card.new
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
 
   def create
     @card = Card.new(card_params)
+    @card.review_date = Date.today + 3
 
-    @card.save
-    redirect_to @card
+
+    if @card.save
+      redirect_to @card
+    else 
+      render 'new'
+    end    
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    @card.review_date = Date.today + 3
+
+    if @card.update(card_params)
+      redirect_to @card
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+
+    redirect_to card_path
   end
 
   private
 
   def card_params
-    params.require(:card).permit(:original_text, :translated_text, :review_date)
+    params.require(:card).permit(:original_text, :translated_text)
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class Card < ApplicationRecord
+  validates :original_text, :translated_text, :review_date, presence: true, length: {minimum: 2}
+
+  validate :original_and_translated_not_equal
+  def original_and_translated_not_equal
+    if original_text == translated_text
+      errors.add(:original_text, "can't be equal")
+    end
+  end
 end
