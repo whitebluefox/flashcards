44fe45aafc4c5f9c75837636f21caf4bfb1a9edb
whitebluefox <kalinkina.nika@mail.ru>
2019-11-21 17:30:29 +0300

    adjustment

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,53 +1,47 @@
 # frozen_string_literal: true
 
 class CardsController < ApplicationController
+  before_action :card, only: [:show, :edit, :update, :destroy]
+
   def index
     @cards = Card.all
   end
 
-  def show
-    @card = Card.find(params[:id])
-  end
+  def show ; end
 
   def new
     @card = Card.new
   end
 
-  def edit
-    @card = Card.find(params[:id])
-  end
+  def edit ; end
 
   def create
-    @card = Card.new(card_params)
-    @card.review_date = Date.today + 3
-
-
     if @card.save
       redirect_to @card
-    else 
+    else
       render 'new'
-    end    
+    end
   end
 
   def update
-    @card = Card.find(params[:id])
-    @card.review_date = Date.today + 3
-
-    if @card.update(card_params)
-      redirect_to @card
+    if card.update(card_params)
+      redirect_to card
     else
       render 'edit'
     end
   end
 
   def destroy
-    @card = Card.find(params[:id])
-    @card.destroy
+    card.destroy
 
     redirect_to card_path
   end
 
   private
+
+  def card
+    @card ||= Card.find(params[:id])
+  end
 
   def card_params
     params.require(:card).permit(:original_text, :translated_text)
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
 class Card < ApplicationRecord
-  validates :original_text, :translated_text, presence: true, length: {minimum: 2}
+  before_validation :review_date_change
+
+  validates :original_text, :translated_text, presence: true,
+                                              length: {minimum: 2}
   validates :review_date, presence: true
 
   validate :original_and_translated_not_equal
+  
   def original_and_translated_not_equal
     if original_text == translated_text
       errors.add(:original_text, "can't be equal")
     end
+  end
+
+private
+
+  def review_date_change
+    self.review_date = Date.today + 3
   end
 end
